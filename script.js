98fec505e940e94b82cb34f189ff2aca6965dbba
Jason <alvarezcoraiza@citypolyhs.org>
2017-07-10 16:02:31 +0000

    new and improved

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // JavaScript File
-// JavaScript File
 /*global $*/
 $( document ).ready(function() {
+    
+    $(".hidden").hide();
+    
     var ra = Math.floor(Math.random()*3);
     if(ra <2){
         $("#general").append("Not much about me other then a general hate for cats.");
@@ -11,6 +13,7 @@ $( document ).ready(function() {
         $("#general").append("Why hello and welcome to my website. If you've come to learn more about me then perhaps you should talk to me instead of staring at a screen like every seems to do these days.");
         $("#why").append("Why don't you take a look and push a button, just for the sake of pushing it. I promise it doesn't do any thing bad. Now about me... For starters I have a dog named Palita and I like to wear sweaters.");
         $("body").attr("background",'http://wallpapercave.com/wp/InfkkQ7.png');
+        $(".hidden").show();
     }
     
     $("button").click(function(){
